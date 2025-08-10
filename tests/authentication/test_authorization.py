c0d5b8a240c9473d9087ef3e011070b35765f22f
GladkiyAQA <d.gladkiy.aqa@gmail.com>
2025-08-10 15:47:57 +0200

    Интеграция Allure в автотесты.Практическая работа с Allure аннотациями

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -18,6 +18,9 @@ from tools.allure.tags import AllureTag
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.AUTHENTICATION)
 @allure.story(AllureStory.AUTHORIZATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
     @pytest.mark.parametrize(
         "email, password",
@@ -71,4 +74,3 @@ class TestAuthorization:
         login_page.click_registration_link()
 
         registration_page.registration_form.check_visible(email="", username="", password="")
-        time.sleep(3)
